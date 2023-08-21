40a12705139d9a63e2c6f21845c1b6eb9f3df445
Wege0921 <wegemuluneh@gmail.com>
2023-08-21 13:46:30 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,67 +1,45 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
-
-#define BUFFER_SIZE 1024
-
-void flush_buffer(char buffer[], int *buffer_index);
-
 /**
- * _printf - Custom printf function
- * @format: Format string containing directives
- * Return: Number of characters printed
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ * @...: The variable number of arguments.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed_chars = 0, chars_printed = 0;
-	int flags, width, precision, size, buffer_index = 0;
 	va_list args;
-	char buffer[BUFFER_SIZE];
-
-	if (format == NULL)
-		return (-1);
+	int printed_chars = 0;
 
 	va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	while (*format)
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			buffer[buffer_index++] = format[i];
-			if (buffer_index == BUFFER_SIZE)
-				flush_buffer(buffer, &buffer_index);
-			chars_printed++;
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 'c')
+				printed_chars += print_char(va_arg(args, int));
+			else if (*format == 's')
+				printed_chars += print_string(va_arg(args, char *));
+			else if (*format == 'd' || *format == 'i')
+				printed_chars += print_number(va_arg(args, int));
+			else if (*format == '%')
+				printed_chars += print_char('%');
 		}
 		else
 		{
-			flush_buffer(buffer, &buffer_index);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, args);
-			precision = get_precision(format, &i, args);
-			size = get_size(format, &i);
-			i++;
-			chars_printed = handle_format(format, &i, args, buffer,
-				flags, width, precision, size);
-			if (chars_printed == -1)
-				return (-1);
-			printed_chars += chars_printed;
+			printed_chars += print_char(*format);
 		}
-	}
 
-	flush_buffer(buffer, &buffer_index);
+		format++;
+	}
 
 	va_end(args);
-return (printed_chars);
-}
 
-/**
- * flush_buffer - Prints the contents of the buffer if it's not empty
- * @buffer: Array of characters
- * @buffer_index: Index to keep track of buffer length
- */
-void flush_buffer(char buffer[], int *buffer_index)
-{
-	if (*buffer_index > 0)
-	{
-		write(1, buffer, *buffer_index);
-		*buffer_index = 0;
-	}
+	return (printed_chars);
 }
