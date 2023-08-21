dc8b915ecaa0dd84f154fc6335627b962c1a4383
Wege0921 <wegemuluneh@gmail.com>
2023-08-21 14:01:55 -0700

    printf old

--- main.h
+++ main.h
@@ -10,26 +10,5 @@ int print_char(char c);
 int print_string(char *str);
 int print_number(int n);
 int _printf(const char *format, ...);
-/**
- * struct fmt - Structure to associate format specifiers with functions
- * @fmt: The format specifier character.
- * @fn: Pointer to the function associated with the format specifier.
- */
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-/**
- * typedef struct fmt - Typedef for struct fmt
- * @fmt: The format specifier character.
- * @fn: Pointer to the function associated with the format specifier.
- */
-typedef struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-} fmt_t;
 
 #endif /* MAIN_H */
